f49ef237c88fdc17a5e5fa9455734159889cdb08
yogeshbpatil <patilyogeshkumarb@gmail.com>
2025-01-25 23:17:52 +0530

    variables

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,1 +1,7 @@
-console.log("Welcine to Appna College");
+// console.log("Welcine to Appna College");
+age = 24;
+price = 99.99;
+x = null;
+fullname = "Tony Stark";
+y = undefined;
+console.log(y);
