d24cf689ed3dc962c577540c099173947996e20b
yogeshbpatil <patilyogeshkumarb@gmail.com>
2025-02-01 08:41:38 +0530

    operators in js

--- Javascript/first.js
+++ Javascript/first.js
@@ -2,6 +2,4 @@ let a = 6;
 let b = 5;
 
 
-console.log("5 >= 5", a >= b)
-// Video no.02 Time : 30.36
-// https://www.youtube.com/watch?v=Zg4-uSjxosE&list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW&index=2
+console.log("!(6<5) = ", !(a===6))
